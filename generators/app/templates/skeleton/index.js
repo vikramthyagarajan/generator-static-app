54673fd09a537266c3c52ef5ed065c6ef1e31be6
Vikram Thyagarajan <vikram@ptotem.com>
2016-01-06 15:51:09 +0530

    Implemented different functionalities for production environments and moved connect-livereload as a dev dependency

--- generators/app/templates/skeleton/index.js
+++ generators/app/templates/skeleton/index.js
@@ -3,7 +3,15 @@ var serveStatic = require('serve-static');
 
 var app = express();
 
-app.use(require('connect-livereload')());
-app.use(serveStatic(__dirname + '/public'));
-console.log('serving app on port 3000. Access the static app at localhost:3000/');
+var env = app.get('env');
+
+if(env == 'production') {
+  console.log('app running in production mode. make sure you"ve run gulp build for your latest code');
+  app.use(serveStatic(__dirname + '/dist'));
+}
+else {
+  app.use(require('connect-livereload')());
+  app.use(serveStatic(__dirname + '/public'));
+}
+console.log('serving app in '+ env +' mode on port 3000. Access the static app at localhost:3000/');
 app.listen(3000);
